3a694b24b75d7913bdfa48ff6ad28532c0681cb4
jerryli12sh <jerryli12.sh@gmail.com>
2025-11-22 22:48:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sunshade-app/', // 必须与GitHub仓库名一致、结尾有斜杠
+  base: '/sunshade_f1/',
   plugins: [react()]
 })
